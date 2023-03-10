597b6874f1f4237f476be2440bd63a8a46e7e4f7
samsamgo <jku2659@gmail.com>
2023-03-11 00:52:41 +0900

    Update Header.js

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useState } from "react";
 import styled from "styled-components";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import "animate.css/animate.min.css";
 
 function Header() {
