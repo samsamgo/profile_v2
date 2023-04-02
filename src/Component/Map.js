d3b3d2bcd36e1e6ef83748ea9fcba8e0df606dde
samsamgo <jku2659@gmail.com>
2023-04-02 23:03:26 +0900

    Update Map.js

--- src/Component/Map.js
+++ src/Component/Map.js
@@ -11,6 +11,7 @@ const MapTest = () => {
     const options = { center: new kakao.maps.LatLng(33.450701, 126.570667) };
     const kakaoMap = new kakao.maps.Map(container, options);
     setMap(kakaoMap);
+    console.log(map);
   }, []);
 
   return (
