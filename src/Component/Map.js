139dc28ee296bd8bfbb5cf21264ebca7479c5af2
samsamgo <jku2659@gmail.com>
2025-04-02 13:13:39 +0900

    지도 수정

--- src/Component/Map.js
+++ src/Component/Map.js
@@ -1,48 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 500px;
 `;
 
 const Map = () => {
-  const [mapLoaded, setMapLoaded] = useState(false);
-
   useEffect(() => {
-    if (window.kakao && window.kakao.maps) {
-      // 이미 로드된 경우
-      loadMap();
-    } else {
-      // 아직 로드 안 됨 - onload 콜백 등록
-      const checkKakao = setInterval(() => {
-        if (window.kakao && window.kakao.maps) {
-          clearInterval(checkKakao);
-          loadMap();
-        }
-      }, 100);
-    }
+    const script = document.createElement("script");
+    script.src =
+      "https://dapi.kakao.com/v2/maps/sdk.js?appkey=e465cf9651ae8fc1554493a6ae90583&autoload=false";
+    script.async = true;
 
-    function loadMap() {
+    script.onload = () => {
       window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById("map");
-        if (!mapContainer) return;
-
+        const container = document.getElementById("map");
         const options = {
           center: new window.kakao.maps.LatLng(37.5665, 126.978),
-          level: 4,
+          level: 3,
         };
-        new window.kakao.maps.Map(mapContainer, options);
-        setMapLoaded(true);
+        new window.kakao.maps.Map(container, options);
       });
-    }
+    };
+
+    document.head.appendChild(script);
   }, []);
 
-  return (
-    <Container id="map">
-      {!mapLoaded && <p style={{ color: "white" }}>지도를 불러오는 중...</p>}
-    </Container>
-  );
+  return <Container id="map">지도를 불러오는 중...</Container>;
 };
 
 export default Map;
